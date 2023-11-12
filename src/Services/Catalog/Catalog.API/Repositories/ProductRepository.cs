a8f2fafa0b0fda57a35096aeb410a10c3fa2ba45
kevin.fernandez <kevin.fernandez@escotel.com.mx>
2023-11-11 19:00:34 -0600

    CatalogService

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Catalog.API.Data;
+using Catalog.API.Entities;
+using Catalog.API.Models;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+  private readonly ICatalogContext _context;
+  private readonly IMapper _mapper;
+
+  public ProductRepository(ICatalogContext context, IMapper mapper)
+  {
+    _context = context;
+    _mapper = mapper;
+  }
+
+  public async Task<IEnumerable<Product>> GetProducts()
+  {
+    return await _context.Products.Find(p => true).ToListAsync();
+  }
+
+  public async Task<Product> GetProduct(string id)
+  {
+    return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+  }
+
+  public async Task<IEnumerable<Product>> GetProductByName(string name)
+  {
+    var filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+    return await _context.Products.Find(filter).ToListAsync();
+  }
+
+  public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+  {
+    var filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+    return await _context.Products.Find(filter).ToListAsync();
+  }
+
+  public async Task<string> CreateProduct(ProductDTO model)
+  {
+    var product = _mapper.Map<Product>(model);
+
+    await _context.Products.InsertOneAsync(product);
+
+    return product.Id;
+  }
+
+  public async Task<bool> UpdateProduct(Product product)
+  {
+    var updateResult = await _context.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+    return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+  }
+
+  public async Task<bool> DeleteProduct(string id)
+  {
+    var filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+    var deleteResult = await _context.Products.DeleteOneAsync(filter);
+
+    return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+  }
+}
---
+++ src/Services/Catalog/Catalog.API/Services/AutoMapperProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Catalog.API.Entities;
+using Catalog.API.Models;
+
+namespace Catalog.API.Services;
+
+public class AutoMapperProfiles : Profile
+{
+  public AutoMapperProfiles()
+  {
+    CreateMap<Product, ProductDTO>().ReverseMap();
+  }
+}
