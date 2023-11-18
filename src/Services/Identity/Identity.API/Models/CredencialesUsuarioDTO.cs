ab362185e21c39578dbc5f61f4378eb1ec26c22d
kevin.fernandez <kevin.fernandez@escotel.com.mx>
2023-11-17 22:40:04 -0600

    Add Identity Service and Discount Service.

---
+++ src/Services/Identity/Identity.API/Models/CredencialesUsuarioDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.API.Models;
+
+public class CredencialesUsuarioDTO
+{
+  [Required]
+  public string UserName { get; set; }
+  [Required]
+  [EmailAddress]
+  public string Email { get; set; }
+}
