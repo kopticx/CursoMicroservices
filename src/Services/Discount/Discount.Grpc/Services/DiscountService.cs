a48c418875eae5c9d444660876187d7b5d64996c
kevin.fernandez <kevin.fernandez@escotel.com.mx>
2023-11-23 11:17:50 -0600

    Refactor Discount service to use PostgreSQL and gRPC
    The Discount service has been refactored to use PostgreSQL as its database and gRPC for service to service communication. The GreeterService and related files have been removed. New classes, files and methods have been introduced related to Discounts, Coupons and their CRUD operations. The Postgres connection string is also added.

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService(IDiscountRepository repository, IMapperBase mapper) : DiscountProtoService.DiscountProtoServiceBase
+{
+  public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+  {
+    var coupon = await repository.GetDiscount(request.ProductName);
+
+    if (coupon is null)
+    {
+      throw new RpcException(new Status(StatusCode.NotFound,
+        $"Discount with ProductName={request.ProductName} is not found"));
+    }
+
+    var couponModel = mapper.Map<CouponModel>(coupon);
+
+    return couponModel;
+  }
+
+  public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+  {
+    var coupon = mapper.Map<Coupon>(request);
+
+    await repository.CreateDiscount(coupon);
+
+    var couponModel = mapper.Map<CouponModel>(coupon);
+
+    return couponModel;
+  }
+
+  public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+  {
+    var coupon = mapper.Map<Coupon>(request);
+
+    await repository.UpdateDiscount(coupon);
+
+    var couponModel = mapper.Map<CouponModel>(coupon);
+
+    return couponModel;
+  }
+
+  public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+  {
+    var response = await repository.DeleteDiscount(request.ProductName);
+
+    var deleteDiscountResponse = mapper.Map<DeleteDiscountResponse>(response);
+
+    return deleteDiscountResponse;
+  }
+}
